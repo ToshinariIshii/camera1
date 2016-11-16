481e71baf842deff207582224afbe3bfc7ae7f54
羽根川拓人 <hanetaku2000@gmail.com>
2016-11-16 18:56:12 +0900

    20161116時点の　メモ以外入力完成かも

--- simplecropview-sample/src/main/java/com/example/simplecropviewsample/SeekBar_Activity.java
+++ simplecropview-sample/src/main/java/com/example/simplecropviewsample/SeekBar_Activity.java
@@ -8,73 +8,66 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 public class SeekBar_Activity extends Fragment {
 
     /*最終的に渡す値*/
-    public static String realTime; // 桁数の関係でStringにした
-    public static int breastMilkValue = 0;
-    public static int milkValue = 0;
+    public static String realTime; // 桁数の関係でStringにした "yyyyMMddHHmm"
+    public static int SeekMilkValue = 0; // シークバーで選択した0~4の整数
+    public static int milkValue = 0; // 「約 ○○ ml」で入力した整数
+    public static String StrCheckedButton; // 母乳 or ミルク
+
 
     private SeekBar seekBar;
-    private TextView seekText1, breastMilk, checkView, yaku, ml, milkView, degree, weight;//seek, breastMilk, check, yaku, ml, milk
+    private TextView seekText1, background1, background2, breastMilk, checkView, yaku, ml, milkView,milkView2;//seek, breastMilk, check, yaku, ml, milk
     private Button cancelButton;
     private Button saveButton;
     static int sbTri;
-    private EditText editText, editWeight, memo;
-    private Spinner spinner;
+    private EditText editText;
+    private RadioGroup milkGroup;
 
     private FloatingActionButton fab;
+    static View v;
 
-    String s = "うわあぁあぁｳﾝﾁｳﾝﾁｳﾝﾁ";
+    String s = "(未入力)";//"うわあぁあぁｳﾝﾁｳﾝﾁｳﾝﾁ";
 
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.activity_seek, container, false);
+        v = inflater.inflate(R.layout.activity_seek, container, false);
         View vtabhost = inflater.inflate(R.layout.activity_tabhost, container, false);
 
         sbTri = 0;//初期化
 
         seekBar = (SeekBar)v.findViewById(R.id.SeekBar01);
+        background1= (TextView)v. findViewById(R.id.background1);
+        background2= (TextView)v. findViewById(R.id.background2);
         seekText1 = (TextView)v. findViewById(R.id.seekText);
         checkView = (TextView)v. findViewById(R.id.checkView1);
         breastMilk = (TextView)v. findViewById(R.id.breastMilkView1);
         milkView = (TextView)v. findViewById(R.id.milkView1);
+        milkView2 = (TextView)v. findViewById(R.id.milkView2);
         yaku = (TextView)v. findViewById(R.id.yaku);
         ml = (TextView)v. findViewById(R.id.ml);
-
-        degree = (TextView)v. findViewById(R.id.degree);
-        weight = (TextView)v. findViewById(R.id.weight);
 
         cancelButton = (Button)v. findViewById(R.id.cancelButton1);
         saveButton = (Button)v. findViewById(R.id.saveButton1);
         editText = (EditText)v. findViewById(R.id.editText);
-        editWeight = (EditText)v. findViewById(R.id.editWeight);
-        memo = (EditText)v. findViewById(R.id.memo);
 
-        spinner = (Spinner)v.findViewById(R.id.spinner2);
+        milkGroup = (RadioGroup)v.findViewById(R.id.milkGroup);
+        // 指定した ID のラジオボタンをチェックします
+        milkGroup.check(R.id.RadioButton1);
+        ///////////
 
         fab = (FloatingActionButton)vtabhost.findViewById(R.id.fab);
 
-
-
-        //TextView textView = (TextView) spinner.getChildAt(0);
-        //textView.setTextColor(Color.rgb(0,0,0));
-
-
-        degree.setVisibility(View.INVISIBLE);
-        weight.setVisibility(View.INVISIBLE);
-        editWeight.setVisibility(View.INVISIBLE);
-        spinner.setVisibility(View.INVISIBLE);
-
-        memo.setVisibility(View.INVISIBLE); // checkView1 の非表示
         checkView.setVisibility(View.INVISIBLE); // checkView1 の非表示
 
         seekText1.setText(s);
@@ -86,7 +79,7 @@ public class SeekBar_Activity extends Fragment {
                         // ツマミをドラッグしたときに呼ばれる
                         /* 渡す値の格納 */
                         s = FunctionsSeek.valueOfBreastMilk(progress);
-                        breastMilkValue = progress;
+                        SeekMilkValue = progress;
                         seekText1.setText(s);
                     }
 
@@ -121,62 +114,46 @@ public class SeekBar_Activity extends Fragment {
     public void ClickSave(View view) {
         switch (sbTri) {
             case 0:
-                //非表示
-                seekText1.setVisibility(View.INVISIBLE);
-                breastMilk.setVisibility(View.INVISIBLE);
-                yaku.setVisibility(View.INVISIBLE);
-                milkView.setVisibility(View.INVISIBLE);
-                seekBar.setVisibility(View.INVISIBLE);
-                editText.setVisibility(View.INVISIBLE);
-                ml.setVisibility(View.INVISIBLE);
-
-                degree.setVisibility(View.VISIBLE);
-                spinner.setVisibility(View.VISIBLE);
-                weight.setVisibility(View.VISIBLE);
-                editWeight.setVisibility(View.VISIBLE);
-
-                sbTri++;
-
-                break;
-
-            case 1:
                 checkView.setVisibility(View.VISIBLE);// checkView1 の表示
-
                 /* 渡す値の格納 */
                 realTime = FunctionsSeek.realTime(1);
                 if(editText.getText().toString().length() != 0)
                     milkValue = Integer.parseInt(editText.getText().toString());
                 else milkValue = 0;
+                RadioButton checkedButton = (RadioButton)v. findViewById(milkGroup.getCheckedRadioButtonId());
+                StrCheckedButton = checkedButton.getText().toString();
 
-                checkView.setText("以下の内容で保存してもよろしいですか？\n\n"
+
+                checkView.setText("\n　　　　　　 [内容確認]\n以下の入力内容で保存してもよろしいでしょうか？\n\n"
                         + "--------------------------------------------------------------\n"
                         + FunctionsSeek.realTime(2) + "\n\n"
-                        + "母乳: " + s + "\n\n"
-                        + "粉ミルク: 約 " + milkValue + " ml\n"
+                        + "摂取物: 「" + StrCheckedButton + "」\n\n"
+                        + "摂取量(主観): 「" + s + "」\n\n"
+                        + "摂取量(測量): 「約 " + milkValue + " ml」\n"
                         + "--------------------------------------------------------------\n");
                 //非表示
                 seekText1.setVisibility(View.INVISIBLE);
+                background1.setVisibility(View.INVISIBLE);
+                background2.setVisibility(View.INVISIBLE);
                 breastMilk.setVisibility(View.INVISIBLE);
                 yaku.setVisibility(View.INVISIBLE);
                 milkView.setVisibility(View.INVISIBLE);
+                milkView2.setVisibility(View.INVISIBLE);
+
                 seekBar.setVisibility(View.INVISIBLE);
                 editText.setVisibility(View.INVISIBLE);
                 ml.setVisibility(View.INVISIBLE);
 
-                degree.setVisibility(View.INVISIBLE);
-                spinner.setVisibility(View.INVISIBLE);
-                weight.setVisibility(View.INVISIBLE);
-                editWeight.setVisibility(View.INVISIBLE);
-
                 ////////
                 fab.setVisibility(View.INVISIBLE);
+                milkGroup.setVisibility(View.INVISIBLE);
 
                 cancelButton.setText("戻る");
                 saveButton.setText("保存");
 
                 sbTri++;
                 break;
-            case 2:
+            case 1:
                 ((MainTabActivity)getActivity()).SeekToast1();
                 break;
             default:
@@ -192,32 +169,27 @@ public class SeekBar_Activity extends Fragment {
             case 1:
                 //非表示
                 seekText1.setVisibility(View.VISIBLE);
+                background1.setVisibility(View.VISIBLE);
+                background2.setVisibility(View.VISIBLE);
                 breastMilk.setVisibility(View.VISIBLE);
                 checkView.setVisibility(View.VISIBLE);
                 yaku.setVisibility(View.VISIBLE);
                 milkView.setVisibility(View.VISIBLE);
+                milkView2.setVisibility(View.VISIBLE);
                 seekBar.setVisibility(View.VISIBLE);
                 editText.setVisibility(View.VISIBLE);
                 ml.setVisibility(View.VISIBLE);
+                milkGroup.setVisibility(View.VISIBLE);
 
                 cancelButton.setText("戻る");
                 saveButton.setText("進む");
                 checkView.setVisibility(View.INVISIBLE);
-                degree.setVisibility(View.INVISIBLE);
-                spinner.setVisibility(View.INVISIBLE);
-                weight.setVisibility(View.INVISIBLE);
-                editWeight.setVisibility(View.INVISIBLE);
 
                 sbTri--;
                 break;
 
             case 2:
                 checkView.setVisibility(View.INVISIBLE);// checkView1 の非表示
-
-                degree.setVisibility(View.VISIBLE);
-                spinner.setVisibility(View.VISIBLE);
-                weight.setVisibility(View.VISIBLE);
-                editWeight.setVisibility(View.VISIBLE);
 
                 cancelButton.setText("戻る");
                 saveButton.setText("進む");
