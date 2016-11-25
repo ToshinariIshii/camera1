207e4b92204e023956e74e959b4094cd14edd29d
Toshinari Ishii <b1014001@fun.ac.jp>
2016-11-25 17:37:58 +0900

    graph_test_committ

--- simplecropview-sample/src/main/java/com/example/simplecropviewsample/Tab1Fragment.java
+++ simplecropview-sample/src/main/java/com/example/simplecropviewsample/Tab1Fragment.java
@@ -5,7 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+//
 public class Tab1Fragment extends Fragment {
     @Override
     public View onCreateView(
